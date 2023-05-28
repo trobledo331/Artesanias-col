00bfc65c739a234b4dd8fafee6ebbe45d7f9b4dc
LuisRuiz <lhruizra@unadvirtual.edu.co>
2023-05-27 20:22:14 -0500

    Cambio

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const App = () => {
   return (
     <div>Inicio del proyecto - Artesanias
-      <h12>Hola soy Luis</h2>
+      <h2>Hola soy Luis</h2>
     </div>
   )
 }

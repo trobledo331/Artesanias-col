05cf079d759bd42535cc89ce60c58eaa99e2ba4f
karizam <karolet2000@gmail.com>
2023-05-27 20:05:16 -0500

    agregar titulo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const App = () => {
   return (
-    <div>Inicio del proyecto - Artesanias</div>
+    <div>Inicio del proyecto - Artesanias
+      <h1>hola soy Karol</h1>
+    </div>
+
+
   )
 }

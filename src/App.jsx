8b4db1a92471bb0334e201f45e1056ac9b004943
karizam <karolet2000@gmail.com>
2023-05-31 11:58:43 -0500

    agregar logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Footer, NavBar } from './components'
 import { BrowserRouter } from 'react-router-dom'
 import { AppRouter } from './router/AppRouter'
+import Logo from './assets/img/Logo/logo.png'
 
 export const App = () => {
   return (
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
+
 export const NavBar = () => {
     return (
         <>
@@ -8,11 +10,11 @@ export const NavBar = () => {
             <div className="row bg-secondary py-2 px-xl-5">
                 <div className="col-lg-6 d-none d-lg-block">
                     <div className="d-inline-flex align-items-center">
-                        <a className="text-dark" href="">FAQs</a>
+                        <a className="text-dark" href="">Preguntas Frecuentes</a>
                         <span className="text-muted px-2">|</span>
-                        <a className="text-dark" href="">Help</a>
+                        <a className="text-dark" href="">Ayuda</a>
                         <span className="text-muted px-2">|</span>
-                        <a className="text-dark" href="">Support</a>
+                        <a className="text-dark" href="">Soporte</a>
                     </div>
                 </div>
                 <div className="col-lg-6 text-center text-lg-right">
@@ -38,7 +40,8 @@ export const NavBar = () => {
             <div className="row align-items-center py-3 px-xl-5">
                 <div className="col-lg-3 d-none d-lg-block">
                     <a href="" className="text-decoration-none">
-                        <h1 className="m-0 display-5 font-weight-semi-bold"><span className="text-primary font-weight-bold border px-3 mr-1">E</span>Shopper</h1>
+                        <h1 className="m-0 display-5 font-weight-semi-bold"><span className="text-primary font-weight-bold border px-3 mr-1">E</span></h1>
+                        <img src={Logo}/>
                     </a>
                 </div>
                 <div className="col-lg-6 col-6 text-left">
